e042d85a2d4e3f72647c0bae51cce8ea81e68794
S MONIKA <monikasuresh2005@gmail.com>
2026-08-31 20:52:24 +0530

    chore: add AppConstants utility class

---
+++ backend/expense-tracker-backend/util/AppConstants.java
@@ -0,0 +1,8 @@
+package com.monika.expensetrackerbackend.util;
+
+public class AppConstants {
+    public static final String DEFAULT_CATEGORY = "OTHER";
+    public static final int MAX_TITLE_LENGTH = 100;
+    public static final int MAX_DESCRIPTION_LENGTH = 500;
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+}
