db2253d0687f054b50bd50d6e24102b47e45ef53
S MONIKA <monikasuresh2005@gmail.com>
2026-09-04 11:51:55 +0530

    chore: add min/max amount constants

--- backend/expense-tracker-backend/util/AppConstants.java
+++ backend/expense-tracker-backend/util/AppConstants.java
@@ -5,4 +5,6 @@ public class AppConstants {
     public static final int MAX_TITLE_LENGTH = 100;
     public static final int MAX_DESCRIPTION_LENGTH = 500;
     public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final Double MIN_AMOUNT = 0.01;
+    public static final Double MAX_AMOUNT = 10000000.0;
 }
